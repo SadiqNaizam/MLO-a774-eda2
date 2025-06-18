6cff51e63642d5f3ff84afe314ac08d278ca22c0
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-18 23:49:38 +0530

    Files generated: Header.tsx

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopHeader from '../Dashboard/TopHeader';
+
+/**
+ * Header component for the main application layout.
+ * It renders the top bar containing page titles and primary actions.
+ */
+const Header: React.FC = () => {
+  return <TopHeader />;
+};
+
+export default Header;
