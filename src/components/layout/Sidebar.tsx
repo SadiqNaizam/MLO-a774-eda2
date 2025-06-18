de6f4402f1c13bf8721d1c731c1b0864b21119b5
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-18 23:49:37 +0530

    Files generated: Sidebar.tsx

---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SidebarNav from '../Dashboard/SidebarNav';
+
+/**
+ * Sidebar component for the main application layout.
+ * It renders the primary navigation panel.
+ */
+const Sidebar: React.FC = () => {
+  return <SidebarNav />;
+};
+
+export default Sidebar;
